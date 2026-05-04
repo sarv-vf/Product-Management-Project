60a51593520e91556fa23ca521001872ab56d87d
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-05-04 17:46:50 +0330

    chore: routes are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterForm from "./pages/RegisterForm";
+import ProductListPage from "./pages/ProductListPage";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <></>;
+  return;
+  <Routes>
+    <Route index element={<Navigate to="/login" replace />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterForm />} />
+    <Route path="/product-list" element={<ProductListPage />} />
+    <Route path="/*" element={<PageNotFound />} />
+  </Routes>;
 }
 
 export default App;
