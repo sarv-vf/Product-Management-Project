75a67a623816ef0f49aeea2c28b3958b03f6bf92
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-05-09 15:38:42 +0330

    feat: showing message fot no products

--- src/components/modules/ProductsTable.jsx
+++ src/components/modules/ProductsTable.jsx
@@ -1,13 +1,22 @@
-import { FaEdit, FaTrashAlt } from "react-icons/fa";
-
 import Loader from "../Loader";
+
+import edit from "../../assets/edit.svg";
+import trash from "../../assets/trash.svg";
+
 import styles from "./ProductsTable.module.css";
+
 function ProductsTable({ products, isLoading, onEdit, onDelete }) {
-  console.log(products);
   return (
     <div className={styles.container}>
       {isLoading ? (
         <Loader />
+      ) : products.length === 0 ? (
+        <div className={styles.emptyWrapper}>
+          <div className={styles.emptyMessage}>
+            <p>هیچ محصولی یافت نشد</p>
+            <span>برای افزودن محصول جدید روی دکمه افزودن کلیک کنید</span>
+          </div>
+        </div>
       ) : (
         <table className={styles.table}>
           <thead>
@@ -16,6 +25,7 @@ function ProductsTable({ products, isLoading, onEdit, onDelete }) {
               <th className={styles.thStock}>موجودی</th>
               <th className={styles.thPrice}>قیمت</th>
               <th className={styles.thId}>شناسه کالا</th>
+              <th className={styles.thActions}></th>
             </tr>
           </thead>
           <tbody>
@@ -27,7 +37,6 @@ function ProductsTable({ products, isLoading, onEdit, onDelete }) {
                 onDelete={onDelete}
               />
             ))}
-            
           </tbody>
         </table>
       )}
@@ -43,22 +52,26 @@ const TableRow = ({ product, onEdit, onDelete }) => {
     <tr>
       <td className={styles.tdName}>{name}</td>
       <td className={styles.tdStock}>{quantity}</td>
-      <td className={styles.tdPrice}>{price?.toLocaleString() || price}</td>
-      <td className={styles.tdId}>{id}</td>
+      <td className={styles.tdPrice}>
+        {price?.toLocaleString() || price} هزار تومان
+      </td>
+      <td className={styles.tdId}>
+        {id ? `${id.slice(0, 8)}...${id.slice(-4)}` : "-"}
+      </td>
       <td className={styles.tdActions}>
         <button
           onClick={() => onEdit(product)}
           className={`${styles.action} ${styles.edit}`}
           title="ویرایش"
         >
-          <FaEdit />
+          <img src={edit} alt="editIcon" className={styles.icon} />
         </button>
         <button
           onClick={() => onDelete(id)}
           className={`${styles.action} ${styles.delete}`}
           title="حذف"
         >
-          <FaTrashAlt />
+          <img src={trash} alt="trashIcon" className={styles.icon} />
         </button>
       </td>
     </tr>
