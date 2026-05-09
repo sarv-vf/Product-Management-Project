d8342783f6d49e6ca63c197be60b0c1e38f131d8
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-05-09 15:39:21 +0330

    feat: pagination component developed

---
+++ src/components/modules/Pagination.jsx
@@ -0,0 +1,32 @@
+import styles from "./Pagination.module.css";
+
+function Pagination({ currentPage,totalPages, onPageChange }) {
+  console.log("Pagination props:", { currentPage , totalPages });
+
+  return (
+    <div className={styles.pagination}>
+      <button
+        onClick={() => onPageChange(1)}
+        className={`${styles.pageBtn} ${currentPage === 1 ? styles.active : ""}`}
+      >
+        ۱
+      </button>
+      
+      <button
+        onClick={() => onPageChange(2)}
+        className={`${styles.pageBtn} ${currentPage === 2 ? styles.active : ""}`}
+      >
+        ۲
+      </button>
+      
+      <button
+        onClick={() => onPageChange(3)}
+        className={`${styles.pageBtn} ${currentPage === 3 ? styles.active : ""}`}
+      >
+        ۳
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
